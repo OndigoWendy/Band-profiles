e0aecb4c85c7c89cce880f59165744289d5297af
OndigoWendy4 <ondigowendy.tech@gmail.com>
2021-12-08 09:48:06 +0300

    Create a grid layout home page and implement newsapi

---
+++ app/src/main/java/com/ondigo/bandheaven/MainActivity.java
@@ -0,0 +1,28 @@
+package com.ondigo.bandheaven;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // get the id of the CardView and attach an onClickListener to it
+        findViewById(R.id.cvNews).setOnClickListener((View.OnClickListener) this);
+    }
+    @Override
+    public void onClick(View view)
+    {
+        if(view.getId() == R.id.cvNews)
+        {
+            //Do something Like starting an activity
+            Intent intent = new Intent(MainActivity.this, NewsActivity.class);
+            startActivity(intent);
+        }
+    }
+}
